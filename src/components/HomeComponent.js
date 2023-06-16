cb62d6cffa5e7d7a340985dab8e63e3bb61d7335
Keroberos26 <jinnguyen054@gmail.com>
2023-06-16 14:53:26 +0700

    Single Page Applications Part 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,12 +1,37 @@
-import React from 'react'
-import { Container } from 'reactstrap'
+import React from "react";
+import { Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Container, Row } from "reactstrap";
 
-function Home() {
+function RenderCard({ item }) {
   return (
-    <Container>
-        <h4>Home</h4>
-    </Container>
-  )
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
-export default Home
+function Home(props) {
+  return (
+    <Container>
+      <Row className="align-items-start">
+        <Col xs={12} md className="m-1">
+          <RenderCard item={props.dish} />
+        </Col>
+        <Col xs={12} md className="m-1">
+          <RenderCard item={props.promotion} />
+        </Col>
+        <Col xs={12} md className="m-1">
+          <RenderCard item={props.leader} />
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Home;
